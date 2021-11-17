c3a09f43ebf5bc8992ddcf721064e475b9a92541
vincepv <vincentpaulvictor@gmail.com>
2021-11-17 18:31:40 +0100

    remove test training

--- test/test_clean_gender.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from module.clean_fr.clean_gender_fr import clean_gender_fr
-
-def test_clean_gender():
-    df_start = pd.DataFrame({'a': [Monsieur, Madame], 'b': [a, b]})
-    df = clean_gender_fr(df_start)
-    df_expect = pd.DataFrame({'a': [2, 1], 'b': [a, b]})
-    assert_frame_equal(df, df_expect)
