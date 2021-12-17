c8d3d4234c765edc9ab063affc1487d8e5505634
vpv <vincentpaulvictor@gmail.com>
2021-12-17 15:18:43 +0100

    autofill change ville

--- module/clean/autofill.py
+++ module/clean/autofill.py
@@ -5,5 +5,5 @@ def autofill(my_file) :
     
     
     df = pd.read_csv(my_file, low_memory=False)
-    df['organisme'] = df['organisme'].ffill(axis=0)
+    df['ville'] = df['ville'].ffill(axis=0)
     df.to_csv(my_pandas_folder+"autofill.csv", header=True, index=False, encoding="utf8")
