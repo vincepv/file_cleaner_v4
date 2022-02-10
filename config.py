afff2b70e59e1af1e0819b0d140f67be7e23b929
vpv <vincentpaulvictor@gmail.com>
2022-02-10 15:35:29 +0100

    add comment

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # Todo : create empty folder in your computer. 
 # This folder receives processed files
 # Put the path of your folder on variable my_pandas_folder
+# Ex. my_pandas_folder = "/yourpath/john/foo/pandas/"
 # Warning ! Backslash on Windows and Forward Slash on macOS and Linux !
 my_pandas_folder = "/Users/VPV/Desktop/pandas/"
 
