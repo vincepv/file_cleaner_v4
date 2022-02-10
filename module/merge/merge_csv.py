e6d2371b6f5b95250fdb464e893dd5190814117d
vpv <vincentpaulvictor@gmail.com>
2022-02-10 14:13:05 +0100

    adress LE fix

--- module/merge/merge_csv.py
+++ module/merge/merge_csv.py
@@ -12,7 +12,7 @@ def merge_csv(folder_csv):
     all_files = glob.glob(folder_csv + "/*.csv")
     li = []
     for filename in all_files:
-        df = pd.read_csv(filename, index_col=None, header=0, sep=',', low_memory=False)
+        df = pd.read_csv(filename, index_col=None, header=0, sep=';', low_memory=False)
         li.append(df)
     df = pd.concat(li, axis=0, ignore_index=True, sort=True)
     df.to_csv(my_pandas_folder+'DraftCsvMerge.csv', encoding= 'utf8', index=False)
