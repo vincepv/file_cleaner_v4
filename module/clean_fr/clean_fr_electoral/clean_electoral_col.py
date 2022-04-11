0cb05dc40203ce5e708d40e629c506f431bf9af0
vpv <vincentpaulvictor@gmail.com>
2022-04-11 18:00:50 +0200

    refactor

--- module/clean_fr/clean_fr_electoral/clean_electoral_col.py
+++ module/clean_fr/clean_fr_electoral/clean_electoral_col.py
@@ -13,16 +13,19 @@ def clean_electoral_col(my_dataframe):
         df.insert(loc=0, column='mot clef', value='')
     if 'sexe' not in df:
         df.insert(loc=0, column='sexe', value='0')
+    
     if 'prenom' not in df:
         df.insert(loc=0, column='prenom', value='prenom a renseigner')
     else:
         df['prenom'] = df['prenom'].fillna('A')
+    
     if 'pays' not in df:
         df.insert(loc=0, column='pays', value='FR')
     else:
         df['pays'] = df['pays'].astype(str)
         df['pays'] = df['pays'].replace(['FRANCE', 'france', 'France'], 'FR')
         df['pays'] = df['pays'].replace(['nan'], 'N/A')
+    
     if 'ville' not in df:
         df.insert(loc=0, column='ville', value='N/A')
     if 'cp' not in df:
@@ -35,6 +38,7 @@ def clean_electoral_col(my_dataframe):
         df.insert(loc=0, column='numero', value='N/A')
     if 'date' not in df:
         df.insert(loc=0, column='date', value='N/A')
+    
     if 'mobile' not in df:
         df.insert(loc=0, column='mobile', value='')
     else:
