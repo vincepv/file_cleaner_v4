e37f2e64000a075780cbe1ed6b3dea4b07f81b93
vpv <vincentpaulvictor@gmail.com>
2022-04-22 11:25:37 +0200

    config refacto

--- module/clean_fr/clean_fr_file/clean_date_fr.py
+++ module/clean_fr/clean_fr_file/clean_date_fr.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from config import DATE_OF_BIRTH
 
 def clean_date_fr(clean_date_df):
     
@@ -6,22 +7,22 @@ def clean_date_fr(clean_date_df):
     df = clean_date_df.copy()
     
     # consider date as str : dd-mm-yyyy    
-    df['date'] = df['date'].astype(str)
+    df[DATE_OF_BIRTH] = df[DATE_OF_BIRTH].astype(str)
     
     df.insert(loc=0, column='jour', value='N/A')
     df.insert(loc=0, column='mois', value='N/A')
     df.insert(loc=0, column='annee', value='N/A')
     
-    df['jour'] = df['date'].str[:2]
-    df['mois'] = df['date'].str[3:-5]
-    df['annee'] = df['date'].str[6:]
+    df['jour'] = df[DATE_OF_BIRTH].str[:2]
+    df['mois'] = df[DATE_OF_BIRTH].str[3:-5]
+    df['annee'] = df[DATE_OF_BIRTH].str[6:]
 
-    df['date'] = df['annee']+ "-" + df['mois']+ "-" + df['jour']
+    df[DATE_OF_BIRTH] = df['annee']+ "-" + df['mois']+ "-" + df['jour']
     # clean invalide date
-    df['date'] = df['date'].replace(['--na', '--N/','--'], 'N/A', regex=True)
+    df[DATE_OF_BIRTH] = df[DATE_OF_BIRTH].replace(['--na', '--N/','--'], 'N/A', regex=True)
     
     df = df.drop(['jour', 'mois', 'annee'], axis=1)
     
     
-    clean_date_done = df['date']
+    clean_date_done = df[DATE_OF_BIRTH]
     return clean_date_done
