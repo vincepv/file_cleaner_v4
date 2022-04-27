4a526b64a94c29be7fac5d82f7dc140228addd00
vpv <vincentpaulvictor@gmail.com>
2022-04-27 11:21:52 +0200

    refacto rename col

--- module/list/rename_column.py
+++ module/list/rename_column.py
@@ -1,19 +1,21 @@
 from config import BIRTH_NAME, CITY, COMMON_NAME, DATE_OF_BIRTH, FIRST_NAME, NUMBER, POLLING_STATION, STREET, ZIP 
 
 rename_column = {
+    "LieuDit":"lieu-dit",
+    "libellé de voie": STREET,
+    "LibelleVoie": STREET,
+    "libelle de voie": STREET,
     "numéro de voie": NUMBER,
     "NumeroVoie": NUMBER,
     "commune": CITY,
+    "code postal": ZIP,
     "code du bureau de vote": POLLING_STATION,
     "NoBureauDeVote": POLLING_STATION,
-    "libellé de voie": STREET,
-    "LibelleVoie": STREET,
-    "code postal": ZIP,
     "date de naissance": DATE_OF_BIRTH,
     "nom d'usage": COMMON_NAME,
     "NomDusage": COMMON_NAME,
     "nom de naissance": BIRTH_NAME,
     "NomdeNaissance": BIRTH_NAME,
     "prénoms": FIRST_NAME,
-    "LieuDit":"lieu-dit",
+    
 }
