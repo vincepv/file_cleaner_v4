2bf1f840d35e6f7b837b1b95af83da9c9397e852
vpv <vincentpaulvictor@gmail.com>
2022-05-05 17:17:59 +0200

    refacto

--- module/clean/clean_header.py
+++ module/clean/clean_header.py
@@ -13,7 +13,7 @@ def clean_header(folder_to_clean):
     for file_path  in all_files:
         
     
-        df = pd.read_csv(file_path, index_col=None, header=0, sep=',', low_memory=False)
+        df = pd.read_csv(file_path, index_col=None, header=0, sep=';', low_memory=False)
         
         df.columns = df.columns.str.strip()
         df.columns = df.columns.str.lower()
--- module/clean_fr/clean_fr_electoral/clean_address.py
+++ module/clean_fr/clean_fr_electoral/clean_address.py
@@ -6,20 +6,22 @@ def clean_address_electoral (my_dataframe):
     df = my_dataframe.copy()
     
     # Need to fix : if address column already exist, it delete address
-    
-    df[NUMBER] = df[NUMBER].astype(str)
-    df[STREET] = df[STREET].astype(str)
-    df[[NUMBER,STREET]] = df[[NUMBER,STREET]].replace('nan', '')
-    df[ADDRESS] = df[NUMBER]+' '+df[STREET]
+    if ADDRESS not in df:
+        df[NUMBER] = df[NUMBER].astype(str)
+        df[STREET] = df[STREET].astype(str)
+        df[[NUMBER,STREET]] = df[[NUMBER,STREET]].replace('nan', '')
+        df[ADDRESS] = df[NUMBER]+' '+df[STREET]
 
-    # clean part
-    clean_character = {
-        ',':'',
-        '^ ':'',
-        ' $':'',
-        '/':' ',
-        '_':' ', 
-    }
+        # clean part
+        clean_character = {
+            ',':'',
+            '^ ':'',
+            ' $':'',
+            '/':' ',
+            '_':' ', 
+        }
 
-    df[ADDRESS] = df[ADDRESS].replace(clean_character, regex=True)
-    return df[ADDRESS]
+        df[ADDRESS] = df[ADDRESS].replace(clean_character, regex=True)
+        return df[ADDRESS]
+    else :
+        return df[ADDRESS]
