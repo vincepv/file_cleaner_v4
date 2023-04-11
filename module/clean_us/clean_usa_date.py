6948a8669641ad3023a0e6b7b3822eb9ca63053c
vpv <support@digitalebox.fr>
2023-04-11 14:37:09 +0200

    refacto date clean

--- module/clean_us/clean_usa_date.py
+++ module/clean_us/clean_usa_date.py
@@ -5,41 +5,13 @@ def clean_usa_date(my_file):
     
     df = pd.read_csv(my_file, low_memory=False)
 
-    df['date'] = df['date'].astype(str)
+    # replace invalid dates with NaN
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
-    # clean month with 2 digits : m to mm
+    # drop rows with NaN dates
+    df = df.dropna(subset=['date'])
 
-    df['date'] = df['date'].str.replace('^1/', '01/', regex=True)
-    df['date'] = df['date'].str.replace('^2/', '02/', regex=True)
-    df['date'] = df['date'].str.replace('^3/', '03/', regex=True)
-    df['date'] = df['date'].str.replace('^4/', '04/', regex=True)
-    df['date'] = df['date'].str.replace('^5/', '05/', regex=True)
-    df['date'] = df['date'].str.replace('^6/', '06/', regex=True)
-    df['date'] = df['date'].str.replace('^7/', '07/', regex=True)
-    df['date'] = df['date'].str.replace('^8/', '08/', regex=True)
-    df['date'] = df['date'].str.replace('^9/', '09/', regex=True)
-
-    # clean day with 2 digits : d to dd
-
-    df['date']=df['date'].str.replace('/1/', '/01/', regex=True)
-    df['date']=df['date'].str.replace('/2/', '/02/', regex=True)
-    df['date']=df['date'].str.replace('/3/', '/03/', regex=True)
-    df['date']=df['date'].str.replace('/4/', '/04/', regex=True)
-    df['date']=df['date'].str.replace('/5/', '/05/', regex=True)
-    df['date']=df['date'].str.replace('/6/', '/06/', regex=True)
-    df['date']=df['date'].str.replace('/7/', '/07/', regex=True)
-    df['date']=df['date'].str.replace('/8/', '/08/', regex=True)
-    df['date']=df['date'].str.replace('/9/', '/09/', regex=True)
-    # create column to receive data clean
-    df.insert(loc=0, column='jour', value='N/A')
-    df.insert(loc=0, column='mois', value='N/A')
-    df.insert(loc=0, column='annee', value='N/A')
-    df['mois'] = df['date'].str[:2]
-    df['jour'] = df['date'].str[3:-5]
-    df['annee'] = df['date'].str[6:]
-    df['date'] = df['annee']+ "-" + df['mois']+ "-" + df['jour']
-    # clean N/A value
-    df['date'] = df['date'].replace(['--na','--N/'],'N/A' , regex=True)
-    df = df.drop(['jour', 'mois', 'annee'], axis=1)
+    # convert dates to dd/mm/yyyy format
+    df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
     df.to_csv(my_pandas_folder+"clean_usa_date.csv",header=True,index=False,encoding="utf8",sep=',')
